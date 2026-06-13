6f127a31a342cbf1d1c799046afa4156b43b05d5
Mangyan <dropproject0001@gmail.com>
2026-06-13 14:32:21 +0800

    .

--- supabase/functions/create-dropper/index.ts
+++ supabase/functions/create-dropper/index.ts
@@ -9,6 +9,7 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
 
 serve(async (req) => {
